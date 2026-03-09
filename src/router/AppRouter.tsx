9513e73991230ec5f4f7a457e233962dfe5b195c
Oscar Principi <principioscar89@gmail.com>
2026-03-09 14:42:01 -0300

    ad scrollstop

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { HashRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from '../components/layout/Navbar'
 import Footer from '../components/layout/Footer'
@@ -7,6 +8,13 @@ import QuienesSomos from '../pages/QuienesSomos'
 import Contacto from '../pages/Contacto'
 import PreguntasFrecuentes from '../pages/PreguntasFrecuentes'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
 
 function AnimatedRoutes() {
   const location = useLocation()
@@ -30,6 +38,7 @@ function AnimatedRoutes() {
 export default function AppRouter() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <div className="app-shell">
         <Navbar />
         <main>
